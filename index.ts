91747ccc4cb8eba1b316b4dbab77691eff34a7cd
CarlonBaird <carlyestrella@live.com>
2022-11-18 13:14:32 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+import { Observable } from 'rxjs';
 
+const interval$ = new Observable<number>((subscriber) => {
+  let counter = 1;
 
+  //emit a value every 2 seconds
+  setInterval(() => {
+    if (counter > 3) subscriber.unsubscribe();
+    else subscriber.next(counter++);
+  }, 2000);
+});
+
+interval$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Unsubscribe'),
+});
